e176613e61a6f09eac8ce61183e998e94c7f9402
Chris Lloyd <chris.lloyd.hi+git@gmail.com>
2022-01-23 13:58:39 +1300

    lamp: read temperature sensor values

--- lamp/README.md
+++ lamp/README.md
@@ -5,3 +5,4 @@
 ## Getting Started
 Install NodeMCU in Arduino Studio
 Install Neopixel Library in Arduino Studio
+Install DHT Sensor Library in Arduino Studio
--- lamp/arduino/lamp/lamp.ino
+++ lamp/arduino/lamp/lamp.ino
@@ -1,40 +1,51 @@
 #include <Adafruit_NeoPixel.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
 
-#define PIN D1
+#define LED_PIN D1
 #define NUM_LEDS 10
 
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRB + NEO_KHZ800);
+#define DHT_PIN 4
+#define DHT_TYPE DHT22
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
+DHT dht(DHT_PIN, DHT_TYPE);
 
 void setup() {
-  pixels.begin();
   Serial.begin(115200);
-  Serial.println("Setup Complete");
+
+  pixels.begin();
+  log("Pixels.begin();");
+  
+  dht.begin();
+  log("dht.begin();");
+
+  Serial.print("");
+  log("Setup Complete.");
 }
 
 void loop() 
 {
-  int led;
-  Serial.println();
-  Serial.print("RED: "); 
-  for(led=0; led <= NUM_LEDS; led++)
-  {
-    setColor(led,255,0,0,100); //red
-    Serial.print(led); Serial.print(", ");
-  }
-  Serial.println();
-  Serial.print("CYAN: "); 
-  for(led=0; led <= NUM_LEDS; led++)
-  {
-    setColor(led,0,255,255,100); //CYAN
-    Serial.print(led); Serial.print(", ");
-  }
-  Serial.println();
-  Serial.print("PURPLE: ");
-  for(led=0; led <= NUM_LEDS; led++)
-  {
-    setColor(led,255,0,255,100); //PURPLE
-    Serial.print(led); Serial.print(", ");
-  }
+    delay(4000);
+    Serial.println();
+    Serial.println("Temperature(*C) Humidity(%)");
+    while(true){
+      // Do LED Things
+      int led;
+      for(led=0; led <= NUM_LEDS; led++)
+      {
+        setColor(led,255,0,0,100); //red
+      }
+    
+      readTemp();
+      
+      for(led=0; led <= NUM_LEDS; led++)
+      {
+        setColor(led,0,255,255,100); //red
+      }
+    
+      readTemp();
+    }
 }
  
 //simple function which takes values for the red, green and blue led and also
@@ -44,4 +55,24 @@ void setColor(int led, int redValue, int greenValue, int blueValue, int delayVal
   pixels.setPixelColor(led, pixels.Color(redValue/5, greenValue/5, blueValue/5)); 
   pixels.show();
   delay(delayValue);
+}
+
+void log(String message){
+  //Serial.println(message)
+}
+
+void readTemp(){
+  log("Reading Temperature");
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
+  if(isnan(temperature)){
+    log("DHT Temperature Read Failed");
+  }else{
+    log(temperature);
+    log(humidity);
+    Serial.print(temperature);
+    Serial.print(" ");
+    Serial.print(humidity);
+    Serial.println();
+  }
 }
