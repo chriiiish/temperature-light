31129a9ba01f72a811f8c5146f11b734eda24808
Chris Lloyd <chris.lloyd.hi+git@gmail.com>
2022-01-23 18:43:29 +1300

    lamp: set LEDs based on room temperature

--- lamp/arduino/lamp/lamp.ino
+++ lamp/arduino/lamp/lamp.ino
@@ -5,11 +5,12 @@
 #define LED_PIN D1
 #define NUM_LEDS 10
 
-#define DHT_PIN 4
+#define DHT_PIN 4 // GPIO - D2
 #define DHT_TYPE DHT22
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
 DHT dht(DHT_PIN, DHT_TYPE);
+
 
 void setup() {
   Serial.begin(115200);
@@ -20,59 +21,74 @@ void setup() {
   dht.begin();
   log("dht.begin();");
 
-  Serial.print("");
   log("Setup Complete.");
 }
 
+
 void loop() 
 {
-    delay(4000);
-    Serial.println();
-    Serial.println("Temperature(*C) Humidity(%)");
-    while(true){
-      // Do LED Things
-      int led;
-      for(led=0; led <= NUM_LEDS; led++)
-      {
-        setColor(led,255,0,0,100); //red
-      }
-    
-      readTemp();
-      
-      for(led=0; led <= NUM_LEDS; led++)
-      {
-        setColor(led,0,255,255,100); //red
-      }
-    
-      readTemp();
-    }
-}
- 
-//simple function which takes values for the red, green and blue led and also
-//a delay
-void setColor(int led, int redValue, int greenValue, int blueValue, int delayValue)
-{
-  pixels.setPixelColor(led, pixels.Color(redValue/5, greenValue/5, blueValue/5)); 
-  pixels.show();
-  delay(delayValue);
+  float temperature = readTemp();
+
+  // Room too cold
+  if (temperature < 20) {
+    setColour(0, 255, 255);  // Cyan
+  }
+  
+  // Room too hot
+  else if (temperature > 22) {
+    setColour(255, 0, 0);  // Red
+  }
+  
+  // Room just right
+  else{
+    setColour(255, 255, 255);  // White
+  }
+
+
+  // Wait for next check
+  delay(1 * 1000);
 }
 
-void log(String message){
+ 
+/*
+ * Sets the colour of the leds in the strip
+ */
+void setColour(int redValue, int greenValue, int blueValue)
+{
+  float brightness = 0.25;
+  
+  for(int led = 0; led < NUM_LEDS; led++){
+    pixels.setPixelColor(led, pixels.Color(redValue * brightness, greenValue * brightness, blueValue * brightness)); 
+  }
+  
+  pixels.show();
+}
+
+
+/*
+ * Logs messages
+ */
+void log(String message)
+{
   //Serial.println(message)
 }
 
-void readTemp(){
+
+/* 
+ *  Read the temperature from the probe, log for plotting
+ */
+float readTemp()
+{
   log("Reading Temperature");
   float temperature = dht.readTemperature();
-  float humidity = dht.readHumidity();
+
   if(isnan(temperature)){
     log("DHT Temperature Read Failed");
-  }else{
+    return 0.0;
+  }
+  else{
     log(temperature);
-    log(humidity);
-    Serial.print(temperature);
-    Serial.print(" ");
-    Serial.print(humidity);
-    Serial.println();
+    Serial.println(temperature);
+    return temperature;
   }
 }
